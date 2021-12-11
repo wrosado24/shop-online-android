46a4ad9c4c316b76e54159f52a924b0998df5b1f
Felipe Rosado <walter.rosado2498@gmail.com>
2021-12-11 04:09:28 -0500

    detalle de producto, funcionando

--- app/src/main/java/com/example/tiendaonline/DetalleProducto.java
+++ app/src/main/java/com/example/tiendaonline/DetalleProducto.java
@@ -3,12 +3,34 @@ package com.example.tiendaonline;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class DetalleProducto extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        String iNombre = getIntent().getExtras().getString("nombreProducto");
+        String idescripcion = getIntent().getExtras().getString("descripcionProducto");
+        String iImagen = getIntent().getExtras().getString("imagenProducto");
+
         setContentView(R.layout.activity_detalle_producto);
+
+        TextView tvNombre = this.findViewById(R.id.txtDpNombre);
+        TextView tvDescripcion = this.findViewById(R.id.txtDpDescripcion);
+
+        tvNombre.setText(iNombre);
+        tvDescripcion.setText(idescripcion);
+
+        imageView = findViewById(R.id.txtDpImagen);
+        Glide.with(this)
+                .load(iImagen)
+                .into(imageView);
     }
 }
